fa5ea0835a713c4d8ee19d66493993d69d3d6e00
Roland Canuto <rcanuto@nexient.com>
2019-11-18 15:08:11 -0500

    added post request

--- src/main/java/roland/roland.java
+++ src/main/java/roland/roland.java
@@ -6,20 +6,15 @@ import static spark.Spark.*;
 public class roland {
 
     public static void main(String... arr) {
-        port(8080);
         get("/", (request, response) -> {
             System.out.println("/");
             response.status(200);
             return "This API is Working.";
         });
-        get("/create", (request, response) -> {
+        post("/create", (request, response) -> {
             System.out.println("/create");
-            JSONObject obj = new JSONObject();
-            obj.put("name", "Roland Canuto");
-            obj.put("age", 25);
-            obj.put("name", "omg");
-            obj.put("age", 22);
-            return obj;
+            Object body = request.body();
+            return body;
         });
     }
 }
