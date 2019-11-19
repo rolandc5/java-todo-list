030fd5af308ec810f2580ed78e1e2bcde8d1a5eb
Roland Canuto <rcanuto@nexient.com>
2019-11-19 15:25:47 -0500

    Added file write to local disk

--- src/main/java/roland/create/Create.java
+++ src/main/java/roland/create/Create.java
@@ -1,5 +1,25 @@
 package roland.create;
 
+import org.json.simple.JSONObject;
+import java.io.File;
+import java.io.IOException;
+
 public class Create {
-    public String name;
+    public static void write(String name, long age) {
+        JSONObject todo = new JSONObject();
+        todo.put("name", name);
+        todo.put("age", age);
+        System.out.println(todo);
+    }
+
+    public static String fileExists() throws IOException {
+        File file = new File("wow.json");
+        if (file.exists() == false) {
+            file.createNewFile();
+            System.out.println("Created file since it didn't exist");
+            return "Successfully Created file";
+        } else {
+            return "File Exists, Reading File";
+        }
+    }
 }
--- src/main/java/roland/roland.java
+++ src/main/java/roland/roland.java
@@ -1,23 +1,38 @@
 package roland;
 
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.File;
+import java.io.IOException;
+
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import static spark.Spark.*;
+import roland.create.Create;
 
 public class roland {
     public static void main(String... arr) {
         JSONParser jsonP = new JSONParser();
+        Create create = new Create();
 
         //Get Request to see if API is working.
         get("/", (request, response) -> {
-            System.out.println("/");
             response.status(200);
             return "This API is Working.";
         });
 
+        get("/retrieveTodo", (request, response) -> {
+            create.fileExists();
+
+            return "Hello There!";
+        });
+
+        // TODO
+        // Add a get request to get todo list.
+        // Use file.Write and store to local disk
+
         //Post request to create a new todo.
         post("/create", (request, response) -> {
-            System.out.println("/create");
             JSONObject body = (JSONObject) jsonP.parse(request.body());
             String name = (String) body.get("name");
             long age = (long) body.get("age");
@@ -25,6 +40,7 @@ public class roland {
                 response.status(400);
                 return "Missing Fields";
             } else {
+                create.write(name, age);
                 response.status(200);
                 return "Created Successfully";
             }
