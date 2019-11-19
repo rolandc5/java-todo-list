45ec81d13e216b67e3070823680a5212959579fd
Roland Canuto <rcanuto@nexient.com>
2019-11-19 14:00:23 -0500

    Added response status and error fields

--- src/main/java/roland/roland.java
+++ src/main/java/roland/roland.java
@@ -7,16 +7,27 @@ import static spark.Spark.*;
 public class roland {
     public static void main(String... arr) {
         JSONParser jsonP = new JSONParser();
+
+        //Get Request to see if API is working.
         get("/", (request, response) -> {
             System.out.println("/");
             response.status(200);
             return "This API is Working.";
         });
+
+        //Post request to create a new todo.
         post("/create", (request, response) -> {
             System.out.println("/create");
             JSONObject body = (JSONObject) jsonP.parse(request.body());
-            System.out.println(body.get("name"));
-            return body;
+            String name = (String) body.get("name");
+            long age = (long) body.get("age");
+            if (name.length() == 0 || age == 0) {
+                response.status(400);
+                return "Missing Fields";
+            } else {
+                response.status(200);
+                return "Created Successfully";
+            }
         });
     }
 }
